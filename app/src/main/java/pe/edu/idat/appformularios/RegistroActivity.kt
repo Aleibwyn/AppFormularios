933e5bfb3c65f61dcee431dab229ce4e6efbbbcf
Aleibwyn <mf-a@outlook.com>
2024-02-12 12:13:35 -0500

    primer commit

---
+++ app/src/main/java/pe/edu/idat/appformularios/RegistroActivity.kt
@@ -0,0 +1,37 @@
+package pe.edu.idat.appformularios
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Toast
+import pe.edu.idat.appformularios.databinding.ActivityRegistroBinding
+
+class RegistroActivity : AppCompatActivity(), OnClickListener {
+    private lateinit var binding: ActivityRegistroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegistroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegistrarUsuario.setOnClickListener(this)
+        binding.btnListarUsuarios.setOnClickListener(this)
+    }
+
+    override fun onClick(vista: View) {
+        when (vista.id) {
+            R.id.btnRegistrarUsuario -> {
+                registrarPersona()
+            }
+
+            R.id.btnListarUsuarios -> {
+                startActivity(Intent(applicationContext, ListadoActivity::class.java))
+            }
+        }
+    }
+
+    fun registrarPersona() {
+        Toast.makeText(applicationContext, "Click en Registro de persona", Toast.LENGTH_LONG).show()
+    }
+}
